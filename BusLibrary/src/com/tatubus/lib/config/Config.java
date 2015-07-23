ef06cb3674ff48f79ff63b7a6aa58619c4153cde
Ho Doan Trung <trunghd2810@gmail.com>
2015-07-23 17:54:09 +0800

    change connection

--- BusLibrary/src/com/tatubus/lib/config/Config.java
+++ BusLibrary/src/com/tatubus/lib/config/Config.java
@@ -4,10 +4,10 @@ package com.tatubus.lib.config;
 public class Config {
     // JDBC driver name and database URL
    public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   public static final String DB_URL = "jdbc:mysql://localhost:8889/BusDB";
+   public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/BusDB";
 
    //  Database credentials
    public static final String USER = "root";
-   public static final String PASS = "root";
+   public static final String PASS = "123456";
    
 }
